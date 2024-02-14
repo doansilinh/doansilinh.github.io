43a027ace4249ccf8ece3d55188d83b9abf13ac6
Linh Luon Leo <linhluonleo04@gmail.com>
2024-02-14 20:23:23 +0700

    .

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("loveBtn").addEventListener("click", function() {
+    document.getElementById("surprise").innerHTML = "<img src='https://placehold.co/600x400' alt='Heart GIF'>";
+    document.getElementById("surprise").style.display = "block";
+});
